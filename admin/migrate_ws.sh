9a448f876d082c11fdcd15c2670d595c10c0f9cb
Shinjae Yoo <sjyoo@bnl.gov>
2014-10-28 15:21:07 +0000

    fixed version issue but still has issues with object reference

--- admin/migrate_ws.sh
+++ admin/migrate_ws.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 w=$1
+ws-url http://localhost:7058; 
 ws-createws $1
 ws-url https://kbase.us/services/ws; 
 for ot in `ws-listobj $w | awk '{if (NR != 1)print $2"_-_"$4}'`; do 
     o=$(echo $ot | sed -r "s/_-_.*$//"); 
-    t=$(echo $ot | sed -r "s/^.*_-_//"); 
+    t=$(echo $ot | sed -r "s/^.*_-_//" | sed -r "s/-[0-9]+\.[0-9]+$//"); 
     echo $o $t; 
     ws-get -w $w $o > $o.json; 
     ws-url http://localhost:7058; 
@@ -12,3 +13,5 @@ for ot in `ws-listobj $w | awk '{if (NR != 1)print $2"_-_"$4}'`; do
     ws-url https://kbase.us/services/ws; 
     rm $o.json;
 done;
+ws-url http://localhost:7058; 
+ws-listobj $w
