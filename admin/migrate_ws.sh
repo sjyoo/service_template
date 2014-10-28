0f94121b18a794e291e3aa21868556b8d2f7430c
Shinjae Yoo <sjyoo@bnl.gov>
2014-10-28 15:10:44 +0000

    added ws migration script

---
+++ admin/migrate_ws.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+w=$1
+ws-createws $1
+ws-url https://kbase.us/services/ws; 
+for ot in `ws-listobj $w | awk '{if (NR != 1)print $2"_-_"$4}'`; do 
+    o=$(echo $ot | sed -r "s/_-_.*$//"); 
+    t=$(echo $ot | sed -r "s/^.*_-_//"); 
+    echo $o $t; 
+    ws-get -w $w $o > $o.json; 
+    ws-url http://localhost:7058; 
+    ws-load $t $o $o.json -w $w; 
+    ws-url https://kbase.us/services/ws; 
+    rm $o.json;
+done;
